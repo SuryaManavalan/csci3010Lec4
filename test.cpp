9517828be7e6971326cf5afc8df1c754b7aa2b7e
SuryaManavalan <surya.mana200@gmail.com>
2023-01-26 13:23:34 -0700

    updated tests

--- test.cpp
+++ test.cpp
@@ -3,34 +3,28 @@
 #include "functions_to_implement.cpp"
 #include "catch.hpp"
 
-//unit test for factorial
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+//unit test for RemoveTwos
+TEST_CASE( "RemoveTwos is computed", "[RemoveTwos]" ) {
+    REQUIRE( RemoveTwos(4) == 1 );
+    REQUIRE( RemoveTwos(7) == 7 );
+    REQUIRE( RemoveTwos(26) == 13 );
+    REQUIRE( RemoveTwos(52) == 13 );
 }
 
-//unit test for nth fibonacci
-TEST_CASE( "nth fibonacci is computed", "[nth fibonacci]" ) {
-    REQUIRE( NthFibonacci(1) == 1 );
-    REQUIRE( NthFibonacci(2) == 1 );
-    REQUIRE( NthFibonacci(3) == 2 );
-    REQUIRE( NthFibonacci(10) == 55 );
+//unit test for multiples filter
+TEST_CASE( "multiples filter is computed", "[multiples filter]" ) {
+    std::vector<int> v1{1,2,3,2,5,5};
+    std::vector<int> res1 = MultiplesFilter(v1, 2);
+    REQUIRE( res1[0] == 1 );
+    REQUIRE( res1[1] == 3 );
+    REQUIRE( res1[2] == 5 );
+    REQUIRE( res1[3] == 5 );
 }
 
 //unit test for even mask
 TEST_CASE( "even mask is computed", "[even mask]" ) {
-    std::vector<int> v1{1,2,3,2,5,5};
     std::vector<int> v2{1,2,3,2,5,5,4};
-    std::vector<bool> res1 = EvenMask(v1);
     std::vector<bool> res2 = EvenMask(v2);
-    REQUIRE( res1[0] == false );
-    REQUIRE( res1[1] == true );
-    REQUIRE( res1[2] == false );
-    REQUIRE( res1[3] == true );
-    REQUIRE( res1[4] == false );
-    REQUIRE( res1[5] == false );
     REQUIRE( res2[0] == false );
     REQUIRE( res2[1] == true );
     REQUIRE( res2[2] == false );
